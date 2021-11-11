e712fb2f2087af5c13a5991f8792065cbbcc94a9
LOGIC50 <ahmedjafrulislam@gmail.com>
2021-11-11 19:27:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require ('dotenv').config();
+const { MongoClient } = require('mongodb');
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lsk7p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log(uri);
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('cameraStore');
+        const productsCollection = database.collection('products');
+        const reviewsCollection = database.collection('reviews');
+
+        // GET PRODUCTS API
+        app.get('/products', async(req, res) => {
+            const cursor = productsCollection.find({});
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+
+        // GET REVIEW API
+        app.get('/reviews', async(req, res) => {
+            const cursor = reviewsCollection.find({});
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        })
+
+        // POST REVIEW API
+        app.post('/reviews', async(req, res) => {
+            const review = req.body;
+            const result = await reviewsCollection.insertOne(review);
+            res.json(review);
+        })
+
+        // POST PRODUCTS API
+        app.post('/products', async(req, res) => {
+            const product = req.body;
+            const result = await productsCollection.insertOne(product);
+            res.json(product);
+        })
+    }
+    finally{
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello Logic Camera Store')
+})
+
+app.listen(port, () => {
+    console.log(`listening at ${port}`)
+});
